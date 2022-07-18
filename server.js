d7421b17e25ff554ad518319f6e4b5c1db2a98e1
BDedafoe <bmd6260@gmail.com>
2022-07-18 18:55:20 -0500

    create and new routes complete

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 //Route
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
