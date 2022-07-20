e32f99b74d05a99b28603d566b93feba98bb74cf
BDedafoe <bmd6260@gmail.com>
2022-07-20 17:22:10 -0500

    delete route complete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Dependencies
 const express = require ('express')
+const methodOverride = require('method-override')
 //Configuration
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -10,6 +11,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
 //Route
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
